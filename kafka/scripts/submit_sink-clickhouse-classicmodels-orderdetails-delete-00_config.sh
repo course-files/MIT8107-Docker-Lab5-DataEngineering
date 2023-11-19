d5745e737174860a30421c533d03d713d60e9ed4
Course Files <137632706+course-files@users.noreply.github.com>
2023-11-19 20:54:20 +0300

    Updated sink connector configs for orderdetails

--- kafka/scripts/submit_sink-clickhouse-classicmodels-orderdetails-delete-00_config.sh
+++ kafka/scripts/submit_sink-clickhouse-classicmodels-orderdetails-delete-00_config.sh
@@ -6,14 +6,14 @@ curl -i -X PUT -H "Content-Type:application/json" \
   http://localhost:8083/connectors/sink-clickhouse-classicmodels-orderdetails-delete-00/config \
   -d '{
             "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
-            "connection.url": "jdbc:clickhouse://clickhouse-server:8123/default",
-            "table.name.format": "payments",
+            "connection.url": "jdbc:clickhouse://clickhouse-server:8123/classicmodels",
+            "table.name.format": "orderdetails",
             "tasks.max": "1",
-            "topics": "classicmodels.classicmodels.payments",
+            "topics": "classicmodels.classicmodels.orderdetails",
             "insert.mode": "insert",
             "pk.mode": "record_key",
             "delete.enabled": "true",
-            "pk.fields": "customerNumber, checkNumber",
+            "pk.fields": "orderNumber, productCode",
             "auto.create": "true",
             "auto.evolve": "true",
             "key.converter": "org.apache.kafka.connect.json.JsonConverter",
--- kafka/scripts/submit_sink-clickhouse-classicmodels-orderdetails-insert-00_config.sh
+++ kafka/scripts/submit_sink-clickhouse-classicmodels-orderdetails-insert-00_config.sh
@@ -6,13 +6,13 @@ curl -i -X PUT -H "Content-Type:application/json" \
   http://localhost:8083/connectors/sink-clickhouse-classicmodels-orderdetails-insert-00/config \
   -d '{
             "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
-            "connection.url": "jdbc:clickhouse://clickhouse-server:8123/default",
-            "table.name.format": "payments",
+            "connection.url": "jdbc:clickhouse://clickhouse-server:8123/classicmodels",
+            "table.name.format": "orderdetails",
             "tasks.max": "1",
-            "topics": "classicmodels.classicmodels.payments",
+            "topics": "classicmodels.classicmodels.orderdetails",
             "insert.mode": "insert",
             "pk.mode": "record_key",
-            "pk.fields": "customerNumber, checkNumber",
+            "pk.fields": "orderNumber, productCode",
             "auto.create": "true",
             "auto.evolve": "true",
             "key.converter": "org.apache.kafka.connect.json.JsonConverter",
