fc8f8d52ebb6c4580216d105a842f8c2275b2ce9
Course Files <137632706+course-files@users.noreply.github.com>
2023-11-19 21:22:16 +0300

    Create sink connector configs for productlines table

---
+++ kafka/scripts/submit_sink-clickhouse-classicmodels-productlines-update-00_config.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+curl -X DELETE http://localhost:8083/connectors/sink-clickhouse-classicmodels-productlines-update-00
+
+curl -i -X PUT -H "Content-Type:application/json" \
+  http://localhost:8083/connectors/sink-clickhouse-classicmodels-productlines-update-00/config \
+  -d '{
+            "connector.class": "io.confluent.connect.jdbc.JdbcSinkConnector",
+            "connection.url": "jdbc:clickhouse://clickhouse-server:8123/classicmodels",
+            "table.name.format": "productlines",
+            "tasks.max": "1",
+            "topics": "classicmodels.classicmodels.productlines",
+            "insert.mode": "update",
+            "pk.mode": "record_key",
+            "pk.fields": "productLine",
+            "auto.create": "true",
+            "auto.evolve": "true",
+            "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+            "value.converter": "org.apache.kafka.connect.json.JsonConverter"
+    }'
